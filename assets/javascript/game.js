c27316dfae64cb7378f87406972696f6d596782e
John Joyce <jjoyce88>
2018-04-09 21:32:22 -0400

    fixed random number generation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -43,7 +43,7 @@ function startRound()
 // Function to generate a random number from a specified range
 function getRandomNumber(min, max)
 {
-	return Math.floor(Math.random() * (max - min)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
